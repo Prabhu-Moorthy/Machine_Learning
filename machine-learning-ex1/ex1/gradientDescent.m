7a3f27f560adda54db81362b0a36be18cc6f6f10
Prabhu Moorthy <prabhu.hotpepper@gmail.com>
2016-03-31 15:20:33 +0000

    after submitting

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -2,7 +2,6 @@ function [theta, J_history] = gradientDescent(X, y, theta, alpha, num_iters)
 
 m = length(y); 
 J_history = zeros(num_iters, 1);
-theta = [0;0];
 
 for iter = 1:num_iters
 	h = X * theta;
