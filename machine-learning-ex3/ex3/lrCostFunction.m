047e5d23f22d712dfa897251b5b8f811e25b5a68
Prabhu D M <prabhu.hotpepper@gmail.com>
2016-06-23 11:47:47 +0000

    assignment 3 completed

--- machine-learning-ex3/ex3/lrCostFunction.m
+++ machine-learning-ex3/ex3/lrCostFunction.m
@@ -13,9 +13,9 @@ theta_reg = [0;theta(2:end)];
 %In the first part(fp) we will compute -ylog(h)
 %In the second part(sp) we will compute (1-y)log(1-h)
 fp = y' * log(h);
-sp = (1-y') * log(1-h)
+sp = (1-y') * log(1-h);
 reg = (lambda/(2*m))*(theta_reg'*theta_reg);
-J = -((1/m)*(fp+sp))+reg
+J = -((1/m)*(fp+sp))+reg;
 
 %So, now we have calculated the cost function J(theta) (J), next we need to calculate the partial derivative of J(theta) (J_Part)
 grad =(1/m)*(X'*(h-y)+lambda*theta_reg);
