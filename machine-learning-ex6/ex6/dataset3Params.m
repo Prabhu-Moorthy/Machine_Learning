b02f2e457644e13e304122d42b96998dc8e3186c
prabhu.hotpepper <prabhu.hotpepper@gmail.com>
2016-08-04 14:26:13 +0000

    partial

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -22,6 +22,23 @@ sigma = 0.3;
 %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+result = [];
+minimum = [0 0 0];
+
+C_vec = [0.01 0.03 0.1 0.3 1 3 10];
+sigma_vec = [0.01 0.03 0.1 0.3 1 3 10];
+
+for c = 1:length(C_vec)
+    for s = 1:length(sigma_vec)
+        model = svmTrain(X, y, C_vec(c), @(x1, x2) gaussianKernel(x1, x2, sigma_vec(s)));
+        predictions = svmPredict(model, Xval);
+        result = [ result; mean(double(predictions ~= yval)) C_vec(c) sigma_vec(s) ];
+    endfor
+endfor
+
+minimum = sortrows(result)(1,:);
+C = minimum(2);
+sigma = minimum(3);
 
 
 
